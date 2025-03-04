8f40d3af55e925b8f4014e99e6911479dd0b658b
Antoine Clement <antoine.c@etik.com>
2025-03-04 14:51:17 +0100

    first view of index

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,7 +1,7 @@
 class InstrumentsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-
+@instruments = Instrument.all
   end
 
   def show
