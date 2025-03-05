b79bff12f8f7c412f9c01f832e908aeb48387ec7
Nabil Ghachem <gach.nabil@gmail.com>
2025-03-05 12:08:57 +0100

    fixing schema file

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2025_03_04_135752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
