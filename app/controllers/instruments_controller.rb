73768cb7b1afc3577a1985e28ec77f8d2d05313a
Jeremy Ligaon <jeremy.ligaon@gmail.com>
2025-03-05 13:08:00 +0100

    initial map config

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,7 +1,17 @@
 class InstrumentsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
-@instruments = Instrument.all
+    @instruments = Instrument.all
+
+    # address is on the instrument's user
+    @markers = @instruments.map(&:user).geocoded.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude
+      }
+    end
+
   end
 
   def show
