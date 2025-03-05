2059945f0e5fcfc15f08efb10fab1ffb26a8f7b9
Nabil Ghachem <gach.nabil@gmail.com>
2025-03-05 12:36:12 +0100

    refactored instruments/show.scss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_04_135752) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_04_161147) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,6 +74,18 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_04_135752) do
     t.index ["user_id"], name: "index_instruments_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "instrument_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["instrument_id"], name: "index_reviews_on_instrument_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -94,4 +106,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_04_135752) do
   add_foreign_key "bookings", "users"
   add_foreign_key "instruments", "categories"
   add_foreign_key "instruments", "users"
+  add_foreign_key "reviews", "instruments"
+  add_foreign_key "reviews", "users"
 end
